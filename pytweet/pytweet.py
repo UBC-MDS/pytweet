7279565be80d9dd0553ddf8b11863f9e1b7ef524
mmyz <yzmarco.ma@gmail.com>
2021-02-27 11:37:59 -0800

    update function specs for get_tweets

--- pytweet/pytweet.py
+++ pytweet/pytweet.py
@@ -7,21 +7,24 @@ consumer_secret = ""
 access_key = ""
 access_secret = ""
 
-def get_tweets(handle):
+def get_tweets(handle, n_tweets=-1):
     """
-    Retreives all tweets of a user given their Twitter handle 
+    Retreives all tweets of a user given their Twitter handle
     (i.e. @elonmusk) through the Twitter API.
 
     Parameters:
     -----------
     handle : string
         The Twitter handle of the user, or aka the username.
+    n_tweets : number
+        The total number of tweets you want to retreive from the user.
+        By default, n_tweets=-1 means retrieving all tweets. 
 
     Returns:
     --------
     tweets : dataframe
         A dataframe of the user's tweets, and the sent times.
     """
-
+    
     # TODO
     return None
