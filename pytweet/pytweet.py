9148a63c0370a437edf414b1a6aa1df04da46e45
yuan xiong <yuan.xiong@botany.ubc.ca>
2021-02-27 09:58:51 -0800

    added function specification for sentiment analysis

--- pytweet/pytweet.py
+++ pytweet/pytweet.py
@@ -25,3 +25,39 @@ def get_tweets(handle):
 
     # TODO
     return None
+
+
+def sentiment_analysis (tweets):
+    """
+    This function first trains a Logistic Regression model based on the Natural Languange Processing (NLP) Kit and use this model to do a sentiment study on the given tweets (input).
+
+    Parameters:
+    -----------
+    tweets : dataframe
+        A dataframe of the user's tweets, and the sent times.
+
+    Returns:
+    --------
+    tweets_senti : dataframe
+        A dataframe contains words that are used, sentiment class, and frenquncy. 
+    """
+    # TODO
+    return None
+
+
+def sentiment_plot (tweets_senti):
+        """
+    This function creats a wordcloud plot based on the tweets_senti data (input).
+
+    Parameters:
+    -----------
+    tweets_senti : dataframe
+        A dataframe contains words that are used, sentiment class, and frenquncy. 
+
+    Returns:
+    --------
+    senti_plots : plot
+        Three plots of wordcloud that correponds to all tweets, tweets with positive sentiment and tweets with negative sentiment.
+    """
+    # TODO
+    return None
