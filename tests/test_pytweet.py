452900780c28fb6b881e190f7826ec910ae7f87b
mmyz <yzmarco.ma@gmail.com>
2021-03-03 17:27:40 -0800

    add beginner tests for get_tweets

--- tests/test_pytweet.py
+++ tests/test_pytweet.py
@@ -3,3 +3,23 @@ from pytweet import pytweet
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_get_tweets():
+    # test output format
+    df = get_tweets('@ShawnMendes')
+    assert type(df) == 'Pandas Dataframe'
+    assert columns(df) == ['ID', 'Time', 'Text']
+
+    # test output status
+    assert get_tweets.status == 200
+
+    # test output length
+    n_tweets = 35
+    df = get_tweets('@ShawnMendes', n_tweets=n_tweets)
+    assert df.shape[0] == n_tweets
+
+def test_your:
+    df = get_tweets
+
+    df 
