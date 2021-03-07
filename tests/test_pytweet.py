b13b19c232b58c541d116e377cbbb3b3a1682a52
yuan xiong <yuanxiongbear@gmail.com>
2021-03-06 19:23:47 -0800

    add tests for sentiment analysis function

--- tests/test_pytweet.py
+++ tests/test_pytweet.py
@@ -147,6 +147,28 @@ def test_plot_hashtags():
     assert plot.encoding.y.shorthand == 'Keyword', 'y_axis should be mapped to the y axis'  
     assert plot.mark == 'bar', 'mark should be a bar'
 
+def test_tweet_sentiment_analysis():
+    """
+    Test existing functionalities of tweet_sentiment_analysis(), which supposed to be a dataframe with semetiment results
+    """
+    # make sure the input is a dataframe
+    assert type(result) == pd.core.frame.DataFrame 
+  
+    # make sure the output has the correct columns 
+    assert sum(result.columns == ['time', 'tweet', 'polarity', 'sentiment', 'neg', 'neu', 'pos', 'compound') 
+    
+    # make sure the output is not empty. 
+    assert len(result) > 0
+
+def test_tweet_sentiment_analysis_error():
+    """
+    Test error cases and error messages thrown by tweet_sentiment_analysis.
+    3 tests in total.
+    """
+    # test invalid input
+    with raises(TypeError) as e:
+        tweet_sentiment_analysis('@ShawnMendes')
+    assert str(e.value) == 'Invalid argument type: input must be a dataframe.'
 
 
 def test_visualize_sentiments():
