93d3128d3b0cb9a688e52f3bb05103d54f717712
lihh <lihuanhuan1003@hotmail.com>
2021-03-25 23:45:54 -0700

    Fix time format in plot_timeline

--- tests/test_plot_timeline.py
+++ tests/test_plot_timeline.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from pytweet.pytweet import plot_timeline
+from pytweet.pytweet import plot_timeline, get_tweets
 from pytest import raises
 
 
@@ -11,8 +11,7 @@ def test_plot_timeline():
     None
         The test should pass and no asserts should be displayed.
     """
-    # Calling helper function to create data
-    data = pd.read_csv("tests/brunomars_data.csv")
+    data = get_tweets('@BrunoMars', n_tweets=500)
 
     # Test the Exception is correctly raised when the type of
     # arguments are wrong
